c1fb0ebd215cc854315a868b8d66f2de16d955db
zinedineBenkhider <46469623+zinedineBenkhider@users.noreply.github.com>
2022-02-22 03:37:36 +0100

    Update PokemonTypeService.java

--- src/main/java/com/ifi/trainer_ui/pokemonType_service/PokemonTypeService.java
+++ src/main/java/com/ifi/trainer_ui/pokemonType_service/PokemonTypeService.java
@@ -2,12 +2,9 @@ package com.ifi.trainer_ui.pokemonType_service;
 
 import com.ifi.trainer_ui.bo.PokemonType;
 import com.ifi.trainer_ui.bo.Trainer;
-
 import java.util.List;
 
 public interface PokemonTypeService {
-
     List<PokemonType> listPokemonsTypes();
     void setListPokemonsTypesForTrainer(Trainer trainer);
-
 }
