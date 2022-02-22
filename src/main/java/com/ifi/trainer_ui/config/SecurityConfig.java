163f43b0384a783d3cd6e9aeff02d642f5d948ea
zinedineBenkhider <46469623+zinedineBenkhider@users.noreply.github.com>
2022-02-22 03:39:11 +0100

    Update SecurityConfig.java

--- src/main/java/com/ifi/trainer_ui/config/SecurityConfig.java
+++ src/main/java/com/ifi/trainer_ui/config/SecurityConfig.java
@@ -7,11 +7,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
 import java.util.Optional;
 
 @Configuration
