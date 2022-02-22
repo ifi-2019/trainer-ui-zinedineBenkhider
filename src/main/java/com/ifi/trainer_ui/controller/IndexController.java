ca55774bea294b271d42be6dc06c518873e8a732
zinedineBenkhider <46469623+zinedineBenkhider@users.noreply.github.com>
2022-02-22 03:35:40 +0100

    Update IndexController.java

--- src/main/java/com/ifi/trainer_ui/controller/IndexController.java
+++ src/main/java/com/ifi/trainer_ui/controller/IndexController.java
@@ -7,15 +7,15 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
+    
     @GetMapping(value = "/")
     public String index() {
-
         return "index";
     }
+    
     @PostMapping(value = "/registerTrainer")
     public ModelAndView registerNewTrainer(String trainerName) {
         ModelAndView modelAndView=new ModelAndView("register");
-
         modelAndView.addObject("name",trainerName);
         return modelAndView;
     }
